990e592e06d4b456b02299b718df75611f06ab4b
alexanderbates <alexander.shakeel.bates@gmail.com>
2019-08-05 14:21:40 +0100

    Create utilities.R

---
+++ R/utilities.R
@@ -0,0 +1,31 @@
+# hidden
+nullToNA <- function(x) {
+  x[sapply(x, is.null)] <- NA
+  return(x)
+}
+changenull <- function(x, to = ""){
+  ifelse(is.null(x),to,x)
+}
+
+# hidden
+ml_progress <- function (x, max = 100, message = "querying ms") {
+  percent <- x / max * 100
+  cat(sprintf('\r|%-50s| ~%d%% %s',
+              paste(rep('+', percent / 2), collapse = ''),
+              floor(percent), message))
+  if (x == max)
+    cat('\n')
+}
+
+# hidden
+FirstLower <- function(x) {
+  s <- strsplit(x, " ")[[1]]
+  paste(tolower(substring(s, 1,1)), substring(s, 2),
+        sep="", collapse=" ")
+}
+
+# hidden
+ml_url <- function(...) {
+  baseurl=getOption("nat.mouselight.url", "http://ml-neuronbrowser.janelia.org")
+  do.call(file.path, c(baseurl, list(...), fsep = "/"))
+}
