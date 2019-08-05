e19b42b6b2cffdce56a50afcdfee14039947796e
alexanderbates <alexander.shakeel.bates@gmail.com>
2019-08-05 15:08:22 +0100

    Update get_meshes.R

--- R/get_meshes.R
+++ R/get_meshes.R
@@ -1,40 +1,49 @@
 #' @title Read the brain mesh and its brain regions from the MouseLight project
 #'
 #' @description Save  the brain mesh and its brain regions from the MouseLight project
-#' @param ... methods passed to \code{ml_fetch}
-#' @param ... methods passed to \code{ml_fetch}
-#' @param ... methods passed to \code{ml_fetch}
+#' @param type whether just to retreive the outer brain mesh, or the brain constituent neuropils
+#' @param progress logical, toggle progress bar
+#' @param save.path if you do not want to save at \code{package_location/inst/exdata} then specify a location
+#' @param Force logical, whether or not to overwrite .obj files stored at \code{save.path} or the default location
 #' @param ... methods passed to \code{ml_fetch}
 #' @return a \code{nat} package \code{\link[nat]{hxsurf}} object, which mimics the Amira surface format, replete with metadata that can be
 #' accessed using \code{$}
-#' @seealso \code{\link{insectbraindb_neuron_info}}, \code{\link{insectbraindb_read_neurons}}, \code{\link{insectbraindb_species_info}}
-#' @inherit ml_brain_info references
 #' @seealso \code{\link{mouselight_read_brain}}, \code{\link{ml_brain_region_info}}
 #' @examples
 #' \dontrun{
-#' ## First we need to download all of the neurons
-#' mbr = ml_brain_region_info()
+#' ## First we can quickly just plot the outer mesh for the brain
+#' outline = ml_read_brain(type = "outline")
+#' plot3d(outline, col = "pink", alpha = 0.3)
 #'
-#' ## Leet's have a look at this data
+#' ## This is cool, but maybe what we really want are its sub-divisions.
+#' mousebrain = ml_read_brain(type = "brain_areas")
+#' clear3d()
+#' plot3d(mousebrain)
+#' ### This takes a long time the first time you call this function.
+#' ### By default .obj files for these brain regions are saved at the location
+#' ### of this package, then following the path inst/exdata/obj and will be
+#' ### read directly from there in future.
+#'
+#' ## What brain regions are on offer?
+#' print(mousebrain$neuropil_full_names)
+#'
+#' ## Or if we want more information, we can get it like this:
+#' mbr = ml_brain_region_info()
 #' View(ml_brain_region_info)
 #'
-#' ## Some brain regions are actually a subcompartment of others in this set.
-#' ### Hmm, so what would be good is if we could see those correspondences
-#' g = ml_brain_graph()
-#'
-#' ## Let's plot these i na way we can easily see!
-#' library(igraph)
-#' f = igraph::layout_with_fr(g)
-#' plot(g, layout = f)
-#'
-#' ## There are a lot of nodes here, we might want to move them about
-#' tkplot(g, layout = f)
+#' ## Perhaps we want to plot just the amygdala?
+#' ### To do this we can do
+#' amygdala.codes = mousebrain$RegionList[grepl("amygdala",
+#' mousebrain$neuropil_full_names,
+#' ignore.case = TRUE)]
+#' plot3d(outline, col = "pink", alpha = 0.1)
+#' plot3d(subset(mousebrain, amygdala.codes), alpha = 0.5)
 #'
 #' }
 #' @references Winnubst, Johan, Erhan Bas, Tiago A. Ferreira, Zhuhao Wu, Michael N. Economo, Patrick Edson, Ben J. Arthur, et al. 2019. “Reconstruction of 1,000 Projection Neurons Reveals New Cell Types and Organization of Long-Range Connectivity in the Mouse Brain.” bioRxiv. https://doi.org/10.1101/537233.
 #' Economo, Michael N., Nathan G. Clack, Luke D. Lavis, Charles R. Gerfen, Karel Svoboda, Eugene W. Myers, and Jayaram Chandrashekar. 2016. “A Platform for Brain-Wide Imaging and Reconstruction of Individual Neurons.” eLife 5 (January): e10566.
 #' @export
-#' @rdname ml_brain_info
+#' @rdname ml_read_brain
 ml_read_brain <- function(type = c("outline", "brain_areas"),
                           progress  = TRUE,
                           save.path = NULL,
@@ -82,7 +91,7 @@ ml_read_brain <- function(type = c("outline", "brain_areas"),
     message = paste("downloading", df$safeName[url],".obj file", sep = " ")
     localfile = paste0(package.path.data,"/",df$geometryFile[url])
     if(!file.exists(localfile) | Force){
-      t = suppresWarnings (try( utils::download.file(urls[url], localfile, mode='wb', quiet = TRUE), silent = TRUE))
+      t = suppressWarnings (try( utils::download.file(urls[url], localfile, mode='wb', quiet = TRUE), silent = TRUE))
       if(inherits(t,'try-error')) {
         message = c("NOTE: unable to download ", df$safeName[url])
         next
@@ -123,18 +132,4 @@ ml_read_brain <- function(type = c("outline", "brain_areas"),
   brain$neuropil_full_names = df$safeName[success]
   brain
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
